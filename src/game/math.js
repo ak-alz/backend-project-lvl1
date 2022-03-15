260b577b1740211107a9ad2fca9716f295af18bd
AK-VoronM <ak.alz@mail.ru>
2022-03-15 16:05:13 +0300

    step10 fix-gct-calculation v1.0

--- src/game/math.js
+++ src/game/math.js
@@ -4,7 +4,7 @@ export const getNumber = () => getRandom(1, 1000);
 
 export const gct = (x, y) => {
   let gctCurrent = 1;
-  for (let i = 1; i < Math.floor(Math.min(x, y) / 2); i += 1) {
+  for (let i = 1; i <= Math.min(x, y); i += 1) {
     if (x % i === 0 && y % i === 0) gctCurrent = i;
   }
   return gctCurrent;
